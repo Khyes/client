3734ad5ef4535388295349e51a8979514328dd21
alexy piche <alexyautumn@gmail.com>
2023-10-21 19:11:24 -0400

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { StreamChat } from 'stream-chat';
 import { ChannelList, Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelListContainer, ChannelContainer } from './components'
+import { ChannelListContainer, ChannelContainer } from './components';
+import './App.css';
 
 const apiKey = '748pzsp6fjk6';
 
