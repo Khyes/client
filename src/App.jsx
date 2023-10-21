a4b39965b501415fe0441569fd65cde501cec514
alexy piche <alexyautumn@gmail.com>
2023-10-21 18:46:05 -0400

    updated for newer version of react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { ChannelList, Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer } from './components'
+
+const apiKey = '748pzsp6fjk6';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Medical Pager Chat App</h1>
+    <div className='app__wrapper'>
+        <Chat client={client} theme='team light'>
+            <ChannelListContainer 
+            />
+            <ChannelContainer 
+            />
+        </Chat>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+document.body.innerHTML = '<div id="root"></div>';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
