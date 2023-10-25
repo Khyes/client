5c5f7fa18cf2e684e82b05275fa838f40917134d
alexy piche <alexyautumn@gmail.com>
2023-10-25 13:22:49 -0400

    added edit channel component

--- src/components/EditChannel.jsx
+++ src/components/EditChannel.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { CloseIcon, useChatContext } from 'stream-chat-react';
 
-const EditChannel = () => {
+import { UserList } from './';
+import { CloseCreateChannel } from '../assets';
+
+const ChannelNameInput = ({ channelName = '', setChannelName}) => {
+  const { client, setActiveChannel } = useChatContext();
+  const [selectedUsers, setSelectedUsers] = useState([client.userID || ''])
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    setChannelName(event.target.value);
+  }
+
   return (
-    <div>EditChannel</div>
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input type="text" value={channelName} onChange={handleChange} placeholder="channel-name (no spaces)" />
+      <p>Add Members</p>
+    </div>
+  )
+}
+
+const EditChannel = ({ setIsEditing }) => {
+  const { channel } = useChatContext();
+  const [channelName, setChannelName] = useState(channel?.data?.name);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+
+  return (
+    <div className='edit-channel__container'>
+      <div className='edit-channel__header'>
+        <p>Edit Channel</p>
+        <CloseIcon setIsEditing={setIsEditing} />
+      </div>
+      <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />
+      <UserList setSelectedUsers={setSelectedUsers} />
+      <div className='edit-channel__button-wrapper'>
+        <p>Save Changes</p>
+      </div>
+    </div>
   )
 }
 
